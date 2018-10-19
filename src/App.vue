c7b5a18d5711379f738a15aac71c35757daa9a96
tanandre <tanneman1@gmail.com>
2018-10-18 21:46:45 -0400

    move to new repo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+   <div id="app">
+      <Tools></Tools>
+   </div>
+</template>
+
+<script>
+	import Tools from './components/Tools.vue'
+
+	 export default {
+		name: 'app',
+		components: {
+			Tools
+		}
+	}
+</script>
+
+<style>
+</style>
